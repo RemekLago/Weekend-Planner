4c811032abcdeee4d7d5b345759e206c1ee0eaab
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-03-01 00:07:57 +0100

    add function to download weather condition from url

--- project/weather.py
+++ project/weather.py
@@ -1,30 +1,35 @@
 from pprint import pprint
 import requests
-import keys
+from keys import APIkey
 
 """https://openweathermap.org/forecast5"""
 
-APIkey = keys.APIkey
-cnt = 16
+"""take geographic coordinates to find a city in the next step"""
 city = "Warsaw"
-country_code = "48"
 limit = 1
+coordinates_city = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit={limit}&appid={APIkey}"
+
+coordinats_to_cityname = requests.get(coordinates_city)
+lat = coordinats_to_cityname.json()[0].get("lat")
+lon = coordinats_to_cityname.json()[0].get("lon")
+
+"""take geographic coordinates to find a city and take parameters from the link"""
+cnt = 8
 units = "metric"
+weather_download = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units={units}&cnt={cnt}&appid={APIkey}"
+weather_city = requests.get(weather_download)
 
-coordinats_to_cityname = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city},{country_code}&limit={limit}&appid={APIkey}")
-pprint(coordinats_to_cityname.json())
 
-city_name = coordinats_to_cityname("name")
-lat = coordinats_to_cityname("lat")
-lon = coordinats_to_cityname("lon")
-cnt = 2
-weather_download = requests.get(f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units={units}&cnt={cnt}&appid={APIkey}")
-
-pprint(weather_download.json())
-
-weather_data = weather_download("list")
-# weather_temperature = weather_data[0]("main")
-# weather_description = weather_data("weather")
-# weather_clouds = weather_data("clouds")
-# weather_wind = weather_data("wind")
-# weather_rain = weather_data("rain")
+"""taking: temperature, weather descriptions, level of clouds, level of wind, level of raining"""
+weather_data = weather_city.json().get("list")
+"""1 day, data from 6am i 12am - it is the 5th sample from day """
+weather_day = weather_data[4].get("dt_txt")
+weather_temperature_min = weather_data[2].get("main").get("temp_min")
+weather_temperature_max = weather_data[4].get("main").get("temp_max")
+weather_description = weather_data[4].get("weather")[0].get("description")
+weather_icon = weather_data[4].get("weather")[0].get("icon")
+pprint(weather_day)
+print(weather_temperature_max)
+print(weather_temperature_min)
+print(weather_description)
+print(weather_icon)
