9c29a320050c5c2cf48eea7385ec276084aade57
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-03-01 19:42:08 +0100

    upgrade file

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from matplotlib import projections
 
 db = SQLAlchemy()
 
@@ -11,10 +13,24 @@ def create_app():
 
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+        
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .additional import additional as additional_blueprint
+    app.register_blueprint(additional_blueprint)
+    
 
     return app
