3c5872b183880b651e6dc620bbeec7772ca8cc24
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-03-06 18:34:23 +0100

    added shell

--- project2/planner.py
+++ project2/planner.py
@@ -1,6 +1,6 @@
-from app import app
-from app.models import User
+from app import app, db
+from app.models import User, ActivityDescription
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'ActivitiesInfo': ActivitiesInfo}
+    return {'db': db, 'User': User, 'ActivityDescription': ActivityDescription}
