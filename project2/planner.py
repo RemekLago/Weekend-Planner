0168496d4802e3d415e2ce990857d63a6747f5c2
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-03-12 11:18:30 +0100

    added upgrade

--- project2/planner.py
+++ project2/planner.py
@@ -1,6 +1,7 @@
 from app import app, db
-from app.models import User, ActivityDescription
+from app.models import User, ActivitiesTable, WeatherTable, IconsTable
+
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'ActivityDescription': ActivityDescription}
+    return {'db': db, 'User': User, 'ActivitiesTable': ActivitiesTable, 'WeatherTable': WeatherTable, 'IconsTable': IconsTable}
