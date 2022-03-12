a7ca3d208ad59aaec731d4b02ee7180abb0856d1
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-03-12 10:59:56 +0100

    added function to add location for weather forecast

--- project2/weather.py
+++ project2/weather.py
@@ -5,44 +5,65 @@ from datetime import datetime
 from app import db
 from app.models import WeatherTable
 
-APIkey = "1fcc3b7ebb293bbe3db4de3086b4d39c"
+"""links to weather API and libray with icons:
+https://openweathermap.org/forecast5
+https://openweathermap.org/api/one-call-api
+https://libraries.io/npm/weathericons
+https://rapidapi.com/wettercom-wettercom-default/api/forecast9/"""
 
-"""https://openweathermap.org/forecast5"""
-"""https://openweathermap.org/api/one-call-api"""
-"""https://libraries.io/npm/weathericons"""
-"""https://rapidapi.com/wettercom-wettercom-default/api/forecast9/"""
+def take_city_name():
+    city_list = []
+    """take city name from list of city names"""
+    with open ("city_names.txt") as file:
+        for row in file:
+            city_list.append(row.strip())
+    return city_list
 
-"""take geographic coordinates to find a city in the next step"""
-city = "Warsaw"
-limit = 1
-coordinates_city = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit={limit}&appid={APIkey}"
-coordinates_city_link = coordinates_city
+def take_coordinates_of_city(city):
+    """take geographic coordinates to find a city name"""
+    # city = take_city_name()
+    limit = 1
+    coordinates_city = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit={limit}&appid={APIkey}"
+    coordinates_city_link = coordinates_city
+    city = city
+    coordinats_to_cityname = requests.get(coordinates_city_link)
+    lat = coordinats_to_cityname.json()[0].get("lat")
+    lon = coordinats_to_cityname.json()[0].get("lon")
 
-coordinats_to_cityname = requests.get(coordinates_city_link)
-lat = coordinats_to_cityname.json()[0].get("lat")
-lon = coordinats_to_cityname.json()[0].get("lon")
+    return lat,lon, city
 
-"""take geographic coordinates to find a city and take parameters from the link"""
-units = "metric"
-part = "current,minutely,hourly,alerts"
 
-weather_download = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units={units}&exclude={part}&appid={APIkey}"
-weather_download_link = weather_download
-weather_data = (requests.get(weather_download_link)).json()
-#print(weather_download_link)
+def take_forecast_for_city(city):
+    """take geographic coordinates to find a city and take parameters from the link"""
+    coordinates = take_coordinates_of_city(city)
+    lat = coordinates[0]
+    lon = coordinates[1]
+    city = coordinates[2]
+    units = "metric"
+    part = "current,minutely,hourly,alerts"
 
-"""taking: temperature, weather descriptions, level of clouds, level of wind, level of raining - for 7 day forecast"""
-def create_dict_with_weather():
+    weather_download = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units={units}&exclude={part}&appid={APIkey}"
+    weather_download_link = weather_download
+    weather_data = (requests.get(weather_download_link)).json()
+    # print(weather_download_link)
+
+    return weather_data, city
+
+
+def create_dict_with_weather(city):
+    """create dictionary with weather conditions for futer export to base, taking: temperature, weather descriptions, level of clouds, level of wind, level of raining - for 8 day forecast"""
+    weather_data = take_forecast_for_city(city)[0]
+    city = take_forecast_for_city(city)[1]
     list_with_weather_day_dict = []
     dict_weekday_name = {
-        "0": "Sunday",
-        "1": "Monday",
-        "2": "Tuesday",
-        "3": "Wednesday",
-        "4": "Thursday",
-        "5": "Friday",
-        "6": "Saturday"
-    }
+        "0": "Monday",
+        "1": "Tuesday",
+        "2": "Wednesday",
+        "3": "Thursday",
+        "4": "Friday",
+        "5": "Saturday",
+        "6": "Sunday"
+        }
     for idx in range (8):
         weather_day_dict = {}
         weather_day_dict["id"] = idx 
@@ -58,16 +79,18 @@ def create_dict_with_weather():
         weather_day_dict["weather_cloud"] = weather_data["daily"][idx]["pop"]
         weather_day_dict["weather_description"] = weather_data["daily"][idx]["weather"][0]["description"]
         weather_day_dict["weather_icon"] = weather_data["daily"][idx]["weather"][0]["icon"]
+        weather_day_dict["weather_location"] = city
         list_with_weather_day_dict.append(weather_day_dict)
-    #pprint(list_with_weather_day_dict) 
+    # pprint(list_with_weather_day_dict)
+    
     return list_with_weather_day_dict
     
-create_dict_with_weather()
+# create_dict_with_weather()
 
-def adding_weather_to_base():
-    input_data = create_dict_with_weather()
-    db.session.query(WeatherTable).delete()
-    db.session.commit()
+def adding_weather_to_base(city):
+    """take list with parameters and add to data base"""
+    input_data = create_dict_with_weather(city)
+    
     for idx in input_data:
         weather = WeatherTable(
             weather_date = idx["weather_date"],
@@ -77,9 +100,21 @@ def adding_weather_to_base():
             weather_wind = idx["weather_wind"],
             weather_cloud  = idx["weather_cloud"],
             weather_description = idx["weather_description"],
-            weather_icon = idx["weather_icon"]
+            weather_icon = idx["weather_icon"],
+            weather_location = idx["weather_location"]
             )
         db.session.add(weather)  
         db.session.commit()
-        # print(weather.weather_day_name)
-adding_weather_to_base()
+        # pprint(weather.weather_location)
+        # pprint(weather.weather_day_name)
+
+
+def adding_data_for_all_cities():
+    db.session.query(WeatherTable).delete()
+    db.session.commit()
+    cities = take_city_name()
+    for idx in cities:
+        take_coordinates_of_city(idx)
+        adding_weather_to_base(idx)
+
+adding_data_for_all_cities()
