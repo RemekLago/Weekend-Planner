c262b1f67f1331a6898625db17e77247b7698edd
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-03-13 22:02:32 +0100

    changes in class activities

--- project2/activities_system_add.py
+++ project2/activities_system_add.py
@@ -13,7 +13,9 @@ def open_file():
             else:    
                 list_descriptions.append(temporary_list)
                 temporary_list = []
+    pprint(list_descriptions)
     return list_descriptions
+
 
 def create_dict_with_descriptions():
     data = open_file()
@@ -24,31 +26,53 @@ def create_dict_with_descriptions():
         dict_with_descriptions["activite_name"] = idx[0]
         dict_with_descriptions["activite_description"] = idx[1]
         dict_with_descriptions["activite_todo_list"] = idx[2]
-        dict_with_descriptions["activite_conditions"] = idx[3]
+        # dict_with_descriptions["activite_conditions"] = idx[3]
         dict_with_descriptions["activite_calories"] = idx[4]
-        dict_with_descriptions["activite_favorite"] = idx[5]
-        dict_with_descriptions["activity_user_id"] = idx[7]
+        dict_with_descriptions["activity_conditions_temp"] = idx[5]
+        dict_with_descriptions["activity_conditions_1"] = idx[6]
+        dict_with_descriptions["activity_conditions_2"] = idx[7]
+        dict_with_descriptions["activity_conditions_3"] = idx[8]
+        dict_with_descriptions["activity_conditions_4"] = idx[9]
+        dict_with_descriptions["activity_conditions_5"] = idx[10]
+        dict_with_descriptions["activity_conditions_6"] = idx[11]
+        dict_with_descriptions["activity_conditions_7"] = idx[12]
+        dict_with_descriptions["activity_conditions_8"] = idx[13]
+        dict_with_descriptions["activity_conditions_9"] = idx[14]
+        dict_with_descriptions["activity_user_id"] = idx[15]
+        dict_with_descriptions["activity_favorite"] = False
         list_with_dict_description.append(dict_with_descriptions)
         dict_with_descriptions = {}
-    #pprint(list_with_dict_description)
+    pprint(list_with_dict_description)
     return list_with_dict_description
 
 def adding_activities_to_base():
     input_data = create_dict_with_descriptions()
     today = datetime.today()
+    # db.session.query(ActivitiesTable).delete()
+    # db.session.commit()
     for idx in input_data:
         activity = ActivitiesTable(
             activity_name = idx["activite_name"],
             activity_description = idx["activite_description"],
             activity_todo_list = idx["activite_todo_list"],
-            activity_conditions = idx["activite_conditions"],
+            # activity_conditions = idx["activite_conditions"],
             activity_calories = idx["activite_calories"],
-            activity_favorite = idx["activite_favorite"],
+            activity_conditions_temp = idx["activity_conditions_temp"],
+            activity_conditions_1 = idx["activity_conditions_1"],
+            activity_conditions_2 = idx["activity_conditions_2"],
+            activity_conditions_3 = idx["activity_conditions_3"],
+            activity_conditions_4 = idx["activity_conditions_4"],
+            activity_conditions_5 = idx["activity_conditions_5"],
+            activity_conditions_6 = idx["activity_conditions_6"],
+            activity_conditions_7 = idx["activity_conditions_7"],
+            activity_conditions_8 = idx["activity_conditions_8"],
+            activity_conditions_9 = idx["activity_conditions_9"],
             activity_user_id = idx["activity_user_id"],
+            activity_favorite = idx["activity_favorite"],
             activity_timestamp = today
             )
         
-        # print(activity.activity_name)
+        # print(activity.activity_conditions_4)
         db.session.add(activity)  
         db.session.commit()
 
