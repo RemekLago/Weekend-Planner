5a64d550b40f7d2da7d003a47cbe8268c1849209
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-03-16 20:29:03 +0100

    update

--- project2/app/__init__.py
+++ project2/app/__init__.py
@@ -5,6 +5,8 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 from flask_moment import Moment
+from flask_pymongo import PyMongo
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,5 +15,14 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 moment = Moment(app)
+# app.config.from_object(PyMongo)
+# ALLOWED_EXTENSIONS = ["png", "jpg", "jpeg", "gif"]
+# # app.config["SECRET_KEY"] = "SECRET_KEY"
+# app.config["UPLOAD_FOLDER"] = "app/static/uploads/"
+# app.config["MONGO_DBNAME"] = "gallery"
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/gallery"
+# mongo = PyMongo(app)
+
+
 
 from app import routes, models
