467df141c795887b79907a30e30bf7c138e92294
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-03-22 16:10:25 +0100

    update

--- project2/app/__init__.py
+++ project2/app/__init__.py
@@ -7,6 +7,8 @@ from config import Config
 from flask_moment import Moment
 from flask_pymongo import PyMongo
 from werkzeug.utils import secure_filename
+from flask_mail import Mail, Message
+from keys import Mailkey
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,14 +17,33 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 moment = Moment(app)
-# app.config.from_object(PyMongo)
-# ALLOWED_EXTENSIONS = ["png", "jpg", "jpeg", "gif"]
-# # app.config["SECRET_KEY"] = "SECRET_KEY"
-# app.config["UPLOAD_FOLDER"] = "app/static/uploads/"
-# app.config["MONGO_DBNAME"] = "gallery"
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/gallery"
-# mongo = PyMongo(app)
 
+
+
+# app.config['MAIL_SERVER']="smtp.gmail.com"
+# app.config['MAIL_PORT'] = 587
+# # app.config['MAIL_PORT'] = 465
+# app.config['MAIL_USE_SSL'] = False
+# app.config['MAIL_USE_TLS'] = True
+# # app.config['MAIL_USE_SSL'] = True
+# # app.config['MAIL_USE_TLS'] = False
+# app.config['MAIL_USERNAME'] = "jacekwacek@gmail.com"
+# app.config['MAIL_PASSWORD'] = f"{Mailkey}"
+# app.config['MAIL_DEFAULT_SENDER'] = "jacekwacek@gmail.com"
+# mail = Mail(app)
+
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+# UPLOAD_FOLDER = 'app/static/uploads/'
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
+# app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+# app.secret_key = "secret key"
+
+UPLOAD_FOLDER = 'app/static/uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+secret_key = "secret key"
 
 
 from app import routes, models
