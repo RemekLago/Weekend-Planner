740c7f0bf307fc2efb520f245491a0536139f14c
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-04-27 23:15:19 +0200

    update

--- project2/app/__init__.py
+++ project2/app/__init__.py
@@ -7,8 +7,6 @@ from flask_moment import Moment
 from flask_mail import Mail, Message
 from socket import gethostname
 
-
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
