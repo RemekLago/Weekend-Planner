46f5aab34cb276b5ded0e25cbc0869f6f254ba5b
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-04-27 18:59:26 +0200

    update

--- project2/planner.py
+++ project2/planner.py
@@ -1,6 +1,8 @@
 from app import app, db
 from app.models import User, ActivitiesTable, WeatherTable, IconsTable
+from weather import adding_data_for_all_cities
 
+adding_data_for_all_cities()
 
 @app.shell_context_processor
 def make_shell_context():
